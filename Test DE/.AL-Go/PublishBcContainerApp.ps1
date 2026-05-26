1787fb8cd5ee5a365a730af9d427595badc6ccd3
Freddy Kristiansen <fkr@freddy.dk>
2026-05-26 20:07:44 +0200

    dump publish

--- Test DE/.AL-Go/PublishBcContainerApp.ps1
+++ Test DE/.AL-Go/PublishBcContainerApp.ps1
@@ -6,6 +6,9 @@ if ($doNotPublishApps) {
     return
 }
 
+Write-Host "PublishBcContainerApp Parameters:"
+$parameters | ConvertTo-Json | Out-Host
+
 $parts = "$ENV:GITHUB_REPOSITORY".Split('/')
 $containerName = "$($parts[0])-$($parts[1])-CICD-$($ENV:GITHUB_RUN_ID)".ToLower()
 
--- Test DE/.AL-Go/RemoveBcContainer.ps1
+++ Test DE/.AL-Go/RemoveBcContainer.ps1
@@ -6,7 +6,10 @@ if ($doNotPublishApps) {
     return
 }
 
+Write-Host "RemoveBcContainer Parameters:"
+$parameters | ConvertTo-Json | Out-Host
+
 $parts = "$ENV:GITHUB_REPOSITORY".Split('/')
 $containerName = "$($parts[0])-$($parts[1])-CICD-$($ENV:GITHUB_RUN_ID)".ToLower()
 
-fkh RemoveContainer --name $containerName
+fkh RemoveContainer --name $containerName --useOIDC
