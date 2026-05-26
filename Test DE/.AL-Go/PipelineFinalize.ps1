3da22e3690ee13edb2b8141a097e2d0964abd223
Freddy Kristiansen <fkr@freddy.dk>
2026-05-26 21:16:22 +0200

    add finalize

---
+++ Test DE/.AL-Go/PipelineFinalize.ps1
@@ -0,0 +1,8 @@
+if ($doNotPublishApps) {
+    return
+}
+
+$parts = "$ENV:GITHUB_REPOSITORY".Split('/')
+$containerName = "$($parts[0])-$($parts[1])-CICD-$($ENV:GITHUB_RUN_ID)".ToLower()
+
+fkh RemoveContainer --name $containerName --useOIDC
--- Test DE/.AL-Go/PublishBcContainerApp.ps1
+++ Test DE/.AL-Go/PublishBcContainerApp.ps1
@@ -12,9 +12,11 @@ $parameters | ConvertTo-Json | Out-Host
 $parts = "$ENV:GITHUB_REPOSITORY".Split('/')
 $containerName = "$($parts[0])-$($parts[1])-CICD-$($ENV:GITHUB_RUN_ID)".ToLower()
 
+Write-Host "Waiting for container $containerName to be ready"
 fkh WaitForContainer --name $containerName --useOIDC
+Write-Host "Container $containerName is ready. Publishing apps."
 foreach($app in $parameters.appFile) {
     Write-Host "Publishing $app to $containerName"
     fkh PublishApp --name $containerName --appFile $app --syncMode ForceSync --sync --install --useOIDC
 }
-
+Write-Host "Finished publishing apps to $containerName"
