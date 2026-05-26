321a507ed782946ee090ebb479a3534c891cccb1
Freddy Kristiansen <fkr@freddy.dk>
2026-05-26 21:27:41 +0200

    import test toolkit

--- Test DE/.AL-Go/ImportTestToolkitToBcContainer.ps1
+++ Test DE/.AL-Go/ImportTestToolkitToBcContainer.ps1
@@ -2,6 +2,7 @@ Param(
     [Hashtable]$parameters
 )
 
+Write-Host "ImportTestToolkitToBcContainer Parameters:"
 $parameters | ConvertTo-Json | Out-Host
 
 if ($doNotPublishApps) {
@@ -12,4 +13,9 @@ $parts = "$ENV:GITHUB_REPOSITORY".Split('/')
 $containerName = "$($parts[0])-$($parts[1])-CICD-$($ENV:GITHUB_RUN_ID)".ToLower()
 
 Write-Host "-includeTestFrameworkOnly:$($parameters.includeTestFrameworkOnly) -includeTestLibrariesOnly:$($parameters.includeTestLibrariesOnly) -includeTestRunnerOnly:$($parameters.includeTestRunnerOnly) -includePerformanceToolkit:$($parameters.includePerformanceToolkit)"
-fkh invokescript --name $containerName --useOIDC --scriptFile (Join-Path $PSScriptRoot "ImportTestToolkit.ps1") --scriptParams "-includeTestFrameworkOnly:$($parameters.includeTestFrameworkOnly) -includeTestLibrariesOnly:$($parameters.includeTestLibrariesOnly) -includeTestRunnerOnly:$($parameters.includeTestRunnerOnly) -includePerformanceToolkit:$($parameters.includePerformanceToolkit)"
+$scriptParams = ''
+if ($parameters.includeTestFrameworkOnly) { $scriptParams += " -includeTestFrameworkOnly" }
+if ($parameters.includeTestLibrariesOnly) { $scriptParams += " -includeTestLibrariesOnly" }
+if ($parameters.includeTestRunnerOnly) { $scriptParams += " -includeTestRunnerOnly" }
+if ($parameters.includePerformanceToolkit) { $scriptParams += " -includePerformanceToolkit" }
+fkh invokescript --name $containerName --useOIDC --scriptFile (Join-Path $PSScriptRoot "ImportTestToolkit.ps1") --scriptParams $scriptParams
