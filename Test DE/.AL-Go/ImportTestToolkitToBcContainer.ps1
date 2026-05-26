0b98c0be5a93f7b4d3043fc97348e508605ceb33
Freddy Kristiansen <fkr@freddy.dk>
2026-05-26 21:43:22 +0200

    import

--- Test DE/.AL-Go/ImportTestToolkitToBcContainer.ps1
+++ Test DE/.AL-Go/ImportTestToolkitToBcContainer.ps1
@@ -12,10 +12,14 @@ if ($doNotPublishApps) {
 $parts = "$ENV:GITHUB_REPOSITORY".Split('/')
 $containerName = "$($parts[0])-$($parts[1])-CICD-$($ENV:GITHUB_RUN_ID)".ToLower()
 
-Write-Host "-includeTestFrameworkOnly:$($parameters.includeTestFrameworkOnly) -includeTestLibrariesOnly:$($parameters.includeTestLibrariesOnly) -includeTestRunnerOnly:$($parameters.includeTestRunnerOnly) -includePerformanceToolkit:$($parameters.includePerformanceToolkit)"
+Write-Host "Waiting for container $containerName to be ready"
+fkh WaitForContainer --name $containerName --useOIDC
+Write-Host "Container $containerName is ready. Importing test toolkit."
+
 $scriptParams = ''
 if ($parameters.includeTestFrameworkOnly) { $scriptParams += " -includeTestFrameworkOnly" }
 if ($parameters.includeTestLibrariesOnly) { $scriptParams += " -includeTestLibrariesOnly" }
 if ($parameters.includeTestRunnerOnly) { $scriptParams += " -includeTestRunnerOnly" }
 if ($parameters.includePerformanceToolkit) { $scriptParams += " -includePerformanceToolkit" }
+Write-Host "ImportTestToolkit $scriptParams"
 fkh invokescript --name $containerName --useOIDC --scriptFile (Join-Path $PSScriptRoot "ImportTestToolkit.ps1") --scriptParams $scriptParams
