11713d87c27e258c641cf586305952aff8d298eb
Freddy Kristiansen <fkr@freddy.dk>
2026-05-26 16:51:54 +0200

    add pipeline init

---
+++ Test DE/.AL-Go/pipelineinitialize.ps1
@@ -0,0 +1,18 @@
+$ENV:FKH_BACKEND_URL = "https://fkh-freddydk-backend.azurewebsites.net/api"
+$ENV:FKH_TIMEZONE = "Europe/Copenhagen"
+
+dotnet tool install -g fkh
+
+$parts = "$ENV:GITHUB_REPOSITORY".Split('/')
+$containerName = "$($parts[0])-$($parts[1])-CICD-$($ENV:GITHUB_RUN_ID)".ToLower()
+
+$adminPassword = GetRandomPassword
+$adminUsername = "admin"
+$info = fkh createcontainer --name $containerName --artifact $artifact --adminUsername $adminUsername --adminPassword $adminPassword --asJson | ConvertFrom-Json
+$bcAuthContext = @{
+    "username" = $adminUsername
+    "password" = ConvertTo-SecureString -String $adminPassword -AsPlainText -Force
+}
+Set-Variable -Name 'bcAuthContext' -value $bcAuthContext -scope 1
+Set-Variable -Name 'environment' -value ($info | ConvertFrom-Json).webclient -scope 1
+
